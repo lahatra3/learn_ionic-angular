d878346e5ecab03bf7885ac958f680387ff32f8d
lahatra3 <lahatrap20.aps2a@gmail.com>
2022-05-08 03:24:06 +0300

    [ADD] capacitor...

---
+++ avotra/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.avotra.angular',
+  appName: 'avotra',
+  webDir: 'dist/avotra',
+  bundledWebRuntime: false
+};
+
+export default config;
